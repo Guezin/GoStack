da7f648befb8a50dd9f8c262218ec462badbe36c
Guezin <leandroguezinjunior@gmail.com>
2019-12-17 11:35:09 -0300

    Tratamento de excecoes com SENTRY, instalamos o pacote express-async-errors e o Youch.

--- modulo03/src/Server.js
+++ modulo03/src/Server.js
@@ -1,31 +1,48 @@
 import express, { json, static } from 'express'
 import { resolve } from 'path'
-
-import Routes from './Routes'
+import * as Sentry from '@sentry/node'
+import Youch from 'youch'
+import 'express-async-errors'
 import './database/connectionDB'
+
+import sentryConfig from './config/sentry'
+import Routes from './Routes'
 
 class Server {
     constructor() {
         this.server = express()
         this.PORT = 3333
 
+        Sentry.init(sentryConfig)
+
         this.middlewares()
         this.routes()
-
+        this.exceptionHandler()
+        
         this.upServer()
     }
-
+    
     upServer() {
         this.server.listen(this.PORT, console.log(`Server running: http://localhost:${this.PORT}`))
     }
-
+    
     middlewares() {
+        this.server.use(Sentry.Handlers.requestHandler())
         this.server.use(json())
         this.server.use('/files', static(resolve(__dirname, '..', 'tmp', 'uploads')))
     }
 
     routes() {
         this.server.use(Routes)
+        this.server.use(Sentry.Handlers.errorHandler())
+    }
+
+    exceptionHandler() {
+        this.server.use(async (err, req, res, next) => {
+            const errors = await new Youch(err, req).toJSON()
+
+            return res.status(500).json(errors)
+        })
     }
 }
 
---
+++ modulo03/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+    dsn: 'https://5fa690a3783d4e4a86e2b42067187401@sentry.io/1859931'
+}
