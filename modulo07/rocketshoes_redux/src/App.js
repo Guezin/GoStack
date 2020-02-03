eace14563799470de70d9a88e80ca9c327c63ea1
Guezin <leandroguezinjunior@gmail.com>
2020-02-03 11:50:45 -0300

    Configuramos as rotas, criamos os estilos globais, criamos o Header.

---
+++ modulo07/rocketshoes_redux/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+import GlobalStyles from './styles/global';
+import Header from './components/Header';
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <GlobalStyles />
+            <Routes />
+            <Header />
+        </BrowserRouter>
+    );
+}
