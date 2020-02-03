619ac71f3fb0c072b50e1c336681246e4d075560
Guezin <leandroguezinjunior@gmail.com>
2020-02-03 13:24:49 -0300

    Estilizamos a pagina home, adicionamos a lib polished para lidar com o escurecimento do botao.

--- modulo07/rocketshoes_redux/src/App.js
+++ modulo07/rocketshoes_redux/src/App.js
@@ -8,9 +8,9 @@ import Header from './components/Header';
 export default function App() {
     return (
         <BrowserRouter>
+            <Header />
             <GlobalStyles />
             <Routes />
-            <Header />
         </BrowserRouter>
     );
 }
--- modulo07/rocketshoes_redux/src/pages/Home/index.js
+++ modulo07/rocketshoes_redux/src/pages/Home/index.js
@@ -1,7 +1,86 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-    return <h1>Home</h1>;
+    return (
+        <ProductList>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-adidas-gamecourt-masculino/04/COL-4560-304/COL-4560-304_detalhe1.jpg?resize=280:280"
+                    alt="Tênis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>R$129,90</span>
+
+                <button type="submit">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-adidas-gamecourt-masculino/04/COL-4560-304/COL-4560-304_detalhe1.jpg?resize=280:280"
+                    alt="Tênis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>R$129,90</span>
+
+                <button type="submit">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-adidas-gamecourt-masculino/04/COL-4560-304/COL-4560-304_detalhe1.jpg?resize=280:280"
+                    alt="Tênis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>R$129,90</span>
+
+                <button type="submit">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-adidas-gamecourt-masculino/04/COL-4560-304/COL-4560-304_detalhe1.jpg?resize=280:280"
+                    alt="Tênis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>R$129,90</span>
+
+                <button type="submit">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-adidas-gamecourt-masculino/04/COL-4560-304/COL-4560-304_detalhe1.jpg?resize=280:280"
+                    alt="Tênis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>R$129,90</span>
+
+                <button type="submit">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+        </ProductList>
+    );
 }
