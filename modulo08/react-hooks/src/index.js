e7798e0d73c94f6ceefd37d238191c30841dcabf
Guezin <leandroguezinjunior@gmail.com>
2020-02-09 14:29:27 -0300

    Entendendo os Hooks: useState, useEffect, useMemo, useCallback... Finalizado.

--- modulo08/react-hooks/src/index.js
+++ modulo08/react-hooks/src/index.js
@@ -2,4 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import GlobalStyles from './styles/global'
+
+ReactDOM.render(
+    <React.Fragment>
+        <App />
+        <GlobalStyles />
+    </React.Fragment>, document.getElementById('root'));
