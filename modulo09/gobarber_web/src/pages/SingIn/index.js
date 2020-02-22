5d5cdcb92da5e5101516d91d73a4bd25130168b0
Guezin <leandroguezinjunior@gmail.com>
2020-02-22 10:48:07 -0300

    Utizando a lib @rocketseat/unform para lidar com os dados do formulario.

--- modulo09/gobarber_web/src/pages/SingIn/index.js
+++ modulo09/gobarber_web/src/pages/SingIn/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
-import logo from '../../assets/logo.svg'
+import logo from '../../assets/logo.svg';
 
 export default function SingIn() {
+	function handleSubmit(data) {
+		console.tron.log(data)
+	}
 	return (
 		<React.Fragment>
 			<img src={logo} alt="Logo Gobarber" />
 
-			<form>
-				<input type="text" placeholder="Seu email..." />
-				<input type="password" placeholder="Sua senha secreta..." />
+			<Form onSubmit={handleSubmit}>
+				<Input name="email" type="text" placeholder="Seu email..." />
+				<Input name="password" type="password" placeholder="Sua senha secreta..." />
 
 				<button type="submit">Acessar</button>
 				<Link to='/register'>Criar conta gratuita</Link>
-			</form>
+			</Form>
 		</React.Fragment>
 	);
 }
--- modulo09/gobarber_web/src/pages/SingUp/index.js
+++ modulo09/gobarber_web/src/pages/SingUp/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '../../assets/logo.svg'
 
 export default function SingUp() {
+	function handleSubmit(data) {
+		console.tron.log(data)
+	}
 	return (
 		<React.Fragment>
 			<img src={logo} alt="Logo Gobarber" />
 
-			<form>
-				<input type="text" placeholder="Nome completo" />
-				<input type="text" placeholder="Seu e-mail" />
-				<input type="password" placeholder="Sua senha secreta" />
+			<Form onSubmit={handleSubmit}>
+				<Input name="name" type="text" placeholder="Nome completo" />
+				<Input name="email" type="text" placeholder="Seu e-mail" />
+				<Input name="password" type="password" placeholder="Sua senha secreta" />
 
 				<button type="submit">Criar conta</button>
 				<Link to='/'>Já tenho conta</Link>
-			</form>
+			</Form>
 		</React.Fragment>
 	);
 }
