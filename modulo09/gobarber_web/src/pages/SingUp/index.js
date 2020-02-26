e0c3e41da504f6e59ce63d5748485eb9e9ffb1bb
Guezin <leandroguezinjunior@gmail.com>
2020-02-26 10:03:56 -0300

    Adicionando a funcionalidade para cadastro de um usuario.

--- modulo09/gobarber_web/src/pages/SingUp/index.js
+++ modulo09/gobarber_web/src/pages/SingUp/index.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
-import logo from '../../assets/logo.svg'
+import logo from '../../assets/logo.svg';
+
+import { signUpRequest } from '../../store/modules/auth/actions';
 
 export default function SingUp() {
 	const schema = Yup.object().shape({
-		name: Yup
-			.string()
-			.required('Campo nome é obrigatório !'),
-		email: Yup
-			.string()
+		name: Yup.string().required('Campo nome é obrigatório !'),
+		email: Yup.string()
 			.email()
 			.required('Campo e-mail é obrigatório !'),
-		password: Yup
-			.string()
+		password: Yup.string()
 			.min(6, 'No minímo 6 caracteres.')
-			.required('Campo senha é obrigatório !')
+			.required('Campo senha é obrigatório !'),
 	});
 
-	function handleSubmit(data) {
-		console.tron.log(data)
+	const dispatch = useDispatch();
+
+	function handleSubmit({ name, email, password }) {
+		dispatch(signUpRequest(name, email, password));
 	}
 
 	return (
@@ -31,10 +32,14 @@ export default function SingUp() {
 			<Form schema={schema} onSubmit={handleSubmit}>
 				<Input name="name" type="text" placeholder="Nome completo" />
 				<Input name="email" type="text" placeholder="Seu e-mail" />
-				<Input name="password" type="password" placeholder="Sua senha secreta" />
+				<Input
+					name="password"
+					type="password"
+					placeholder="Sua senha secreta"
+				/>
 
 				<button type="submit">Criar conta</button>
-				<Link to='/'>Já tenho conta</Link>
+				<Link to="/">Já tenho conta</Link>
 			</Form>
 		</React.Fragment>
 	);
