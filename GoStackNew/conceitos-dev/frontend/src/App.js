9e37fb2eb1086a867ff8c8ff52ed0687df7475e6
Guezin <leandroguezinjunior@gmail.com>
2020-04-14 16:03:06 -0300

    Aprendemos o conceito de Estado & Imutabilidade.

--- GoStackNew/conceitos-dev/frontend/src/App.js
+++ GoStackNew/conceitos-dev/frontend/src/App.js
@@ -3,21 +3,5 @@ import React from 'react';
 import Header from './components/Header';
 
 export default function App() {
-  return (
-    <>
-      <Header title="Homepage">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
-      <Header title="Projects">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-          <li>Sair</li>
-        </ul>
-      </Header>
-    </>
-  );
+  return <Header title="Projects" />;
 }
--- GoStackNew/conceitos-dev/frontend/src/components/Header.js
+++ GoStackNew/conceitos-dev/frontend/src/components/Header.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function({ title, children }) {
+export default function({ title }) {
+  const [project, setProject] = useState(['Desenvolvimento de app', 'Front-end web']);
+  
+  function handleProject() {
+    return setProject([...project, `Novo Projeto ${new Date().getTime()}`])
+  }
+
   return (
     <header>
       <h1>{ title }</h1>
+      <ul>{project.map(projects => (
+        <li key={projects}>{ projects }</li>
+      ))}
+      </ul>
 
-      { children }
+      <button type="button" onClick={handleProject}>Adicionar projeto</button>
     </header>
   );
 }
