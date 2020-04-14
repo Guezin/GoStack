9d8cabaaba9140610284a42258343eeec507b88a
Guezin <leandroguezinjunior@gmail.com>
2020-04-14 16:34:23 -0300

    Configurando o webpack para compilar arquivos css e imagens.

--- GoStackNew/conceitos-dev/frontend/src/App.js
+++ GoStackNew/conceitos-dev/frontend/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import './App.css'
+import backgroundImage from './assets/background.jpg'
+
 import Header from './components/Header';
 
 export default function App() {
-  return <Header title="Projects" />;
+  return (
+    <>
+      <Header title="Projects" />
+
+      <img src={backgroundImage} alt=""/>
+    </>
+  );
 }
