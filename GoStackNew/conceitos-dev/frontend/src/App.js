0fbe94379371449532ad2623825e38369bc87754
Guezin <leandroguezinjunior@gmail.com>
2020-04-15 09:44:41 -0300

    Listando repositorios da API.

--- GoStackNew/conceitos-dev/frontend/src/App.js
+++ GoStackNew/conceitos-dev/frontend/src/App.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from './services/api';
 
 import './App.css'
-import backgroundImage from './assets/background.jpg'
+//import backgroundImage from './assets/background.jpg'
 
 import Header from './components/Header';
 
 export default function App() {
+  const [projects, setProject] = useState([]);
+  
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setProject(response.data);
+    })
+  }, []);
+
+  function handleProject() {
+    return setProject([...projects, `Novo Projeto ${new Date().getTime()}`])
+  }
+
   return (
     <>
       <Header title="Projects" />
 
-      <img src={backgroundImage} alt=""/>
+      <ul>{projects.map(project => (
+        <li key={project.id}>
+          <strong>{project.title}</strong>
+          <p>URL: {project.url}</p>
+          <p>Techs: {project.techs}</p>
+        </li>
+      ))}
+      </ul>
+
+      <button type="button" onClick={handleProject}>Adicionar projeto</button>
+      
     </>
   );
 }
--- GoStackNew/conceitos-dev/frontend/src/components/Header.js
+++ GoStackNew/conceitos-dev/frontend/src/components/Header.js
@@ -1,21 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function({ title }) {
-  const [project, setProject] = useState(['Desenvolvimento de app', 'Front-end web']);
-  
-  function handleProject() {
-    return setProject([...project, `Novo Projeto ${new Date().getTime()}`])
-  }
-
   return (
     <header>
       <h1>{ title }</h1>
-      <ul>{project.map(projects => (
-        <li key={projects}>{ projects }</li>
-      ))}
-      </ul>
-
-      <button type="button" onClick={handleProject}>Adicionar projeto</button>
     </header>
   );
 }
