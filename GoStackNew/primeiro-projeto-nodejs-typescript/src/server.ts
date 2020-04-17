690d092a848fd718ca5a088b0c77a19f9bd5f330
Guezin <leandroguezinjunior@gmail.com>
2020-04-17 12:29:38 -0300

    Continuando a configuracao para editorConfig eslint prettier.

--- GoStackNew/primeiro-projeto-nodejs-typescript/src/server.ts
+++ GoStackNew/primeiro-projeto-nodejs-typescript/src/server.ts
@@ -1,11 +1,10 @@
-import express from 'express';
+import express from 'express'
+import routes from './routes'
 
 const server = express()
 
-server.get('/', (request, response) => {
-  return response.json({ message: 'Hello Word' });
-});
+server.use(routes)
 
 server.listen(3333, () => console.log('Server started on port 3333.'))
 
-export default server;
+export default server
