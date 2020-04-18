6df92bc15f320dc99327e5122ecb208b68173adb
Guezin <leandroguezinjunior@gmail.com>
2020-04-18 10:34:09 -0300

    Cadastro de agendamento, validando a data, model de agendamento.

--- GoStackNew/primeiro-projeto-nodejs-typescript/src/server.ts
+++ GoStackNew/primeiro-projeto-nodejs-typescript/src/server.ts
@@ -1,8 +1,9 @@
-import express from 'express'
+import express, { json } from 'express'
 import routes from './routes'
 
 const server = express()
 
+server.use(json())
 server.use(routes)
 
 server.listen(3333, () => console.log('Server started on port 3333.'))
