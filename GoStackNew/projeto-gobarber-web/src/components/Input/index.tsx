18b27508a36816a2fa784763b58611ac2e757892
Guezin <leandroguezinjunior@gmail.com>
2020-05-08 17:31:31 -0300

    Isolamos componentes Input e Button, criamos pagina de cadastro, aprendemos utilizar a lib Unform.

---
+++ GoStackNew/projeto-gobarber-web/src/components/Input/index.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/prop-types */
+import React, { useEffect, InputHTMLAttributes, useRef } from 'react';
+import { IconBaseProps } from 'react-icons';
+import { useField } from '@unform/core';
+
+import { Container } from './styles';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon: React.ComponentType<IconBaseProps>;
+}
+
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = useRef(null);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container>
+      {Icon && <Icon size={20} />}
+      <input {...rest} ref={inputRef} />
+    </Container>
+  );
+};
+
+export default Input;
