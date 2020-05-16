d1e6b23aa3cbdb2fabb54da0925cf4bb5b0e99eb
Guezin <leandroguezinjunior@gmail.com>
2020-05-16 14:21:04 -0300

    Estilizando a tela de autenticacao

--- GoStackNew/projeto-gobarber-mobile/src/pages/SignIn/index.tsx
+++ GoStackNew/projeto-gobarber-mobile/src/pages/SignIn/index.tsx
@@ -1,15 +1,54 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-    </Container>
+    <ScrollView
+      contentContainerStyle={{ flex: 1 }}
+      keyboardShouldPersistTaps="handled"
+    >
+      <Container>
+        <Image source={logoImg} />
+
+        <Title>Faça seu logon</Title>
+
+        <Input name="email" icon="mail" placeholder="E-mail" />
+        <Input name="password" icon="lock" placeholder="Senha" />
+
+        <Button
+          onPress={() => {
+            console.log('deu');
+          }}
+        >
+          Entrar
+        </Button>
+
+        <ForgotPassword>
+          <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+        </ForgotPassword>
+
+        <CreateAccountButton>
+          <Icon name="log-in" size={20} color="#ff9000" />
+
+          <CreateAccountButtonText>Criar conta</CreateAccountButtonText>
+        </CreateAccountButton>
+      </Container>
+    </ScrollView>
   );
 };
 
