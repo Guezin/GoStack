287dd44d73ccbaa9cb7dd6d3f3d11408afb18134
Guezin <leandroguezinjunior@gmail.com>
2020-05-16 14:43:30 -0300

    Criamos a tela de cadastro

--- GoStackNew/projeto-gobarber-mobile/src/pages/SignIn/index.tsx
+++ GoStackNew/projeto-gobarber-mobile/src/pages/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Image, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -17,6 +18,8 @@ import {
 import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
     <ScrollView
       contentContainerStyle={{ flex: 1 }}
@@ -42,7 +45,7 @@ const SignIn: React.FC = () => {
           <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
         </ForgotPassword>
 
-        <CreateAccountButton>
+        <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
           <Icon name="log-in" size={20} color="#ff9000" />
 
           <CreateAccountButtonText>Criar conta</CreateAccountButtonText>
