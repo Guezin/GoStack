b2dc190f0336b5609a057ff6df372d8a54b4dd2d
Guezin <leandroguezinjunior@gmail.com>
2020-06-14 14:32:04 -0300

    Listagem dos agendamentos

--- GoStackNew/projeto-gobarber-web/src/pages/Dashboard/index.tsx
+++ GoStackNew/projeto-gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,21 +1,25 @@
-import React, { useCallback } from 'react';
-import { useHistory } from 'react-router-dom';
-import { FiPower } from 'react-icons/fi';
+import React, { useState } from 'react';
+import { FiPower, FiClock } from 'react-icons/fi';
 
 import { useAuth } from '../../hooks/auth';
 
 import logoImg from '../../assets/logo.svg';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+} from './styles';
 
 const Dashboard: React.FC = () => {
   const { signOut, user } = useAuth();
-  const history = useHistory();
-
-  const handleSignOut = useCallback(() => {
-    signOut();
-
-    history.push('/');
-  }, [signOut, history]);
+  const [selectedDate, setSelectDate] = useState(new Date());
 
   return (
     <Container>
@@ -36,6 +40,102 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-Feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atentidemento a seguir</strong>
+            <div>
+              <img
+                src="http://localhost:3333/files/0435bfcf5aad080d3495-20200330_101837.jpg"
+                alt="Leandro Guezin Jr"
+              />
+
+              <strong>Leandro Guezin Jr</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="http://localhost:3333/files/0435bfcf5aad080d3495-20200330_101837.jpg"
+                  alt="Leandro Guezin Jr"
+                />
+
+                <strong>Leandro Guezin Jr</strong>
+                <span>
+                  <FiClock />
+                  08:00
+                </span>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="http://localhost:3333/files/0435bfcf5aad080d3495-20200330_101837.jpg"
+                  alt="Leandro Guezin Jr"
+                />
+
+                <strong>Leandro Guezin Jr</strong>
+                <span>
+                  <FiClock />
+                  08:00
+                </span>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="http://localhost:3333/files/0435bfcf5aad080d3495-20200330_101837.jpg"
+                  alt="Leandro Guezin Jr"
+                />
+
+                <strong>Leandro Guezin Jr</strong>
+                <span>
+                  <FiClock />
+                  08:00
+                </span>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
