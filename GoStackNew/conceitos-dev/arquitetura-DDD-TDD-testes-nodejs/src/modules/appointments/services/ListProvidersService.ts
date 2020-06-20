6590ede82a511673eebaeb019af7edf87a6ee2e5
Guezin <leandroguezinjunior@gmail.com>
2020-06-20 11:44:41 -0300

    acertando alguns detalhes

--- GoStackNew/conceitos-dev/arquitetura-DDD-TDD-testes-nodejs/src/modules/appointments/services/ListProvidersService.ts
+++ GoStackNew/conceitos-dev/arquitetura-DDD-TDD-testes-nodejs/src/modules/appointments/services/ListProvidersService.ts
@@ -1,6 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-
-import AppError from '@shared/errors/AppError';
+import { classToClass } from 'class-transformer';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 
@@ -22,16 +21,19 @@ class ListProvidersService {
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    let users = await this.cacheProvider.recover<User[]>(
-      `provider-list:${user_id}`,
-    );
-
+    // let users = await this.cacheProvider.recover<User[]>(
+    //   `provider-list:${user_id}`,
+    // );
+    let users;
     if (!users) {
       users = await this.usersRepository.findAllProviders({
         except_user_id: user_id,
       });
 
-      await this.cacheProvider.save(`provider-list:${user_id}`, users);
+      await this.cacheProvider.save(
+        `provider-list:${user_id}`,
+        classToClass(users),
+      );
     }
 
     return users;
