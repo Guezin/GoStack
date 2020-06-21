63d0d22f506d49d550f59fd5055a376251cc5471
Guezin <leandroguezinjunior@gmail.com>
2020-06-21 13:38:24 -0300

    Atualizacao do avatar

--- GoStackNew/projeto-gobarber-mobile/src/pages/Profile/index.tsx
+++ GoStackNew/projeto-gobarber-mobile/src/pages/Profile/index.tsx
@@ -5,6 +5,8 @@ import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 import Icon from 'react-native-vector-icons/Feather';
+import ImagePicker from 'react-native-image-picker';
+// import ImageEditor from '@react-native-community/image-editor';
 
 import api from '../../services/api';
 
@@ -38,6 +40,39 @@ const Profile: React.FC = () => {
   const confirmPasswordInputRef = useRef<TextInput>(null);
   const navigation = useNavigation();
   const { user, updateUser } = useAuth();
+
+  const handleUpdateAvatar = useCallback(() => {
+    ImagePicker.showImagePicker(
+      {
+        title: 'Selecione um avatar',
+        cancelButtonTitle: 'Cancelar',
+        takePhotoButtonTitle: 'Usar camêra',
+        chooseFromLibraryButtonTitle: 'Escolher da galeria',
+      },
+      response => {
+        if (response.didCancel) {
+          return;
+        }
+
+        if (response.error) {
+          Alert.alert('Erro ao atualizar seu avatar');
+          return;
+        }
+
+        const data = new FormData();
+
+        data.append('avatar', {
+          type: 'image/jpeg',
+          name: `${user.id}.jpg`,
+          uri: response.uri,
+        });
+
+        api.patch('/users/avatar', data).then(apiResponse => {
+          updateUser(apiResponse.data);
+        });
+      }
+    );
+  }, [updateUser, user.id]);
 
   const handleGoToBack = useCallback(() => {
     navigation.goBack();
@@ -114,7 +149,7 @@ const Profile: React.FC = () => {
         );
       }
     },
-    [navigation]
+    [navigation, updateUser]
   );
 
   return (
@@ -127,7 +162,7 @@ const Profile: React.FC = () => {
           <Icon name="chevron-left" size={24} color="#999591" />
         </BackButton>
 
-        <UserAvatarButton onPress={() => { }}>
+        <UserAvatarButton onPress={handleUpdateAvatar}>
           <UserAvatar source={{ uri: user.avatar_url }} />
         </UserAvatarButton>
 
